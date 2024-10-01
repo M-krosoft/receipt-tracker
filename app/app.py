6550aadcb7e06fcb7908786a30c33a2c1d145867
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-10-01 18:28:36 +0200

    add sqlalchemy, fix packaging

---
+++ app/app.py
@@ -0,0 +1,28 @@
+import logging
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from .config import Config
+
+logger = logging.getLogger(__name__)
+db = SQLAlchemy()
+
+
+def create_app(config: Config):
+    setup_logging_level(config)
+    logger.info("creating app...")
+    logger.debug(f"config=\n {str(config)}")
+
+    _app = Flask(__name__)
+    _app.config.from_object(config)
+    db.init_app(_app)
+
+    return _app
+
+
+def setup_logging_level(config: Config):
+    if config.DEBUG:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
--- run.py
+++ run.py
@@ -1,6 +1,11 @@
-from app import create_app
+from flask import jsonify
+from sqlalchemy import text
 
-app = create_app()
+from app import create_app, db
+from app.config import DevelopmentConfig
+
+config = DevelopmentConfig()
+app = create_app(config)
 
 
 @app.route('/isRunning')
@@ -8,5 +13,15 @@ def home():
     return "Application is running!"
 
 
+@app.route('/test_connection', methods=['GET'])
+def test_db_connection():
+    with app.app_context():
+        try:
+            db.session.execute(text("SELECT 1"))
+            return jsonify({"status": "success", "message": "Connected to the database!"}), 200
+        except Exception as e:
+            return jsonify({"status": "error", "message": str(e)}), 500
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
